8e8ccca753b0d20b03c6eb9d2a0cdcd88cf354fa
Danitaufiqq <91050642+Danitaufiqq@users.noreply.github.com>
2021-10-16 23:43:47 +0700

    Add files via upload

---
+++ PERTEMUAN 2/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+
+ 	public function index()
+ 	{
+ 		echo "<h1>Perkenalkan</h1>";
+ 		echo"Nama saya Muhammad Dani
+			 Saya tinggal di daerah ceger
+			 olah raga yang saya sukai adalah
+ 			 Bulutangkis dan Bola";
